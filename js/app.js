15d4a931294f8b49d2b2c8c7cfab0a15d1e5c024
JaJamaRu <shimizu@ray-up.com>
2021-09-06 20:05:14 +0900

    [Change]  index.html and app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 const billInput = document.querySelector('#bill');
 const tipBtn = document.querySelectorAll('.tip');
+const customTipInput = document.querySelector('#custom-tip-percentage');
 const numOfPeople = document.querySelector('#number-of-people');
 const tipAmountDisplay = document.querySelector('.tip-amount');
 const totalAmountDisplay = document.querySelector('.total-amount');
 const resetBtn = document.querySelector('.reset');
+const inputs = document.querySelectorAll('.input');
 
 // Get bill amount
 let billAmount;
+
 billInput.addEventListener('blur', (event) => {
   billAmount = parseFloat(billInput.value);
   calcResult();
@@ -14,6 +17,7 @@ billInput.addEventListener('blur', (event) => {
 
 // Get tip percentage
 let tipPercentage;
+
 tipBtn.forEach((element) => {
   element.addEventListener('click', (e) => {
     tipPercentage = parseInt(element.value) / 100;
@@ -21,8 +25,18 @@ tipBtn.forEach((element) => {
   });
 });
 
+customTipInput.addEventListener('blur', (event) => {
+  if (parseInt(customTipInput.value) === 0) {
+    tipPercentage = 0;
+  } else {
+    tipPercentage = parseInt(customTipInput.value) / 100;
+  }
+  calcResult();
+});
+
 // Get number of people
 let headCount;
+
 numOfPeople.addEventListener('blur', (event) => {
   headCount = parseInt(numOfPeople.value);
   calcResult();
@@ -31,24 +45,25 @@ numOfPeople.addEventListener('blur', (event) => {
 // Reset everything when reset button is clicked
 resetBtn.addEventListener('click', (event) => {
   billAmount = 0;
-  tipPercentage = '';
+  tipPercentage = 0;
+  headCount = 0;
   billInput.value = '';
+  customTipInput.value = '';
   numOfPeople.value = '';
   tipAmountDisplay.innerHTML = '$0.00';
   totalAmountDisplay.innerHTML = '$0.00';
 });
 
 // Reset input value on focus
-billInput.addEventListener('focus', (event) => {
-  billInput.value = '';
-});
-numOfPeople.addEventListener('focus', (event) => {
-  numOfPeople.value = '';
+inputs.forEach((input) => {
+  input.addEventListener('focus', function (event) {
+    this.value = '';
+  });
 });
 
 // Calculate the results and display them
 const calcResult = () => {
-  if (billAmount && tipPercentage && headCount) {
+  if (billAmount && tipPercentage >= 0 && headCount) {
     let tipPerPerson = (billAmount * tipPercentage) / headCount;
     let billPerPerson = (billAmount + billAmount * tipPercentage) / headCount;
     // cent単位で切り捨て
